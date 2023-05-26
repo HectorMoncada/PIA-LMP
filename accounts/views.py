0afb77cd8c389fc020fa6622693f504778a742e7
José Luis Díaz Camacho <jose.diazcmc@uanl.edu.mx>
2023-05-26 10:59:44 -0600

    Checkout del carrito de compra

--- accounts/views.py
+++ accounts/views.py
@@ -12,6 +12,7 @@ from django.core.mail import EmailMessage
 
 from carts.views import _cart_id
 from carts.models import Cart, CartItem
+import requests
 
 # Create your views here.
 def register(request):
@@ -63,15 +64,53 @@ def login(request):
                 cart= Cart.objects.get(cart_id=_cart_id(request))
                 is_cart_item_exists=CartItem.objects.filter(cart=cart).exists()
                 if is_cart_item_exists:
-                    cart_item=CartItem.objects(cart=cart)
+                    cart_item=CartItem.objects.filter(cart=cart)
+
+                    product_variation = []
                     for item in cart_item:
-                        item.user=user
-                        item.save()
+                        variation=item.variations.all()
+                        product_variation.append(list(variation))
+
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list =[]
+                    id=[]
+                    for item in cart_item:
+                        existing_variation=item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id=id[index]
+                            item=CartItem.objects.get(id=item_id)
+                            item.quantity +=1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user=user
+                                item.save()
+
+
             except:
-                password
+                pass
             auth.login(request, user)
             messages.success(request, 'Has iniciado sesion exitosamente')
-            return redirect('dashboard')
+
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query =requests.utils.urlparse(url).query
+                params=dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+
+
+            except:
+                return redirect('dashboard')
+
         else:
             messages.error(request, 'Las credenciales son incorrectas')
             return redirect('login')
