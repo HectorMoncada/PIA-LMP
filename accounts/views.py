10a6d40f7a4be00ce9f4630553cf288a7eb11fbb
José Luis Díaz Camacho <jose.diazcmc@uanl.edu.mx>
2023-05-26 09:57:09 -0600

    commit antes de la targedia

--- accounts/views.py
+++ accounts/views.py
@@ -10,6 +10,9 @@ from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
 from django.core.mail import EmailMessage
 
+from carts.views import _cart_id
+from carts.models import Cart, CartItem
+
 # Create your views here.
 def register(request):
     form = RegistrationForm()
@@ -56,6 +59,16 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
+            try:
+                cart= Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists=CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item=CartItem.objects(cart=cart)
+                    for item in cart_item:
+                        item.user=user
+                        item.save()
+            except:
+                password
             auth.login(request, user)
             messages.success(request, 'Has iniciado sesion exitosamente')
             return redirect('dashboard')
--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product, Variation
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def _cart_id(request):
@@ -127,3 +128,28 @@ def cart(request, total=0, quantity=0, cart_items=None):
     }
 
     return render(request, 'store/cart.html', context)
+@login_required(login_url='login')
+def checkout(request, total=0, quantity=0, cart_items=None):
+    tax = 0
+    grand_total = 0
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (2*total)/100
+        grand_total = total + tax
+
+    except ObjectDoesNotExist:
+        pass ## solo ignora la exception
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax' : tax,
+        'grand_total': grand_total,
+    }
+
+    return render(request, 'store/checkout.html', context)
